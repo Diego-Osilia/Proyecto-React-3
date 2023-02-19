57f5acd1b0974c21069ed64f9720ec420c4c6477
Diego-Osilia <diegoalejandrosilia@gmail.com>
2023-02-19 16:40:04 -0300

    Creado el diseño

--- proyecto3/src/componentes/Boton.js
+++ proyecto3/src/componentes/Boton.js
@@ -1,10 +1,15 @@
-import react from "react";
+import React from "react";
+import "../hojas-de-estilo/boton.css"
 
 function Boton(props){
 
+    const esOperador = valor => {
+        return isNaN(valor) && (valor != '.') && (valor != '=')
+    };
+
     return(
 
-        <div>
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ""}`.trimEnd()}>
             {props.children}
 
         </div>
